b11d589a78548726f05cec55ead5ec0555cf8053
khalidswe <khalid35-2520@diu.edu.bd>
2021-09-18 15:18:43 +0600

    problems added

---
+++ 3.arithmatic_operator.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+'''
+# also accepted but
+print(a+b)
+print(a-b)
+print(a*b)
+'''
+print(a+b,a-b,a*b,sep ='\n') #new
---
+++ 4.division.py
@@ -0,0 +1,6 @@
+a,b = int(input()),int(input())
+
+first = a//b
+sec = float (a/b)
+
+print(first,sec,sep='\n')
